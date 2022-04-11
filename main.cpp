2c08864d4a5ef54148191bf9aca2e71d0d439ab2
João Vítor <joao.barbosa@tcsindustrial.com.br>
2022-04-11 16:16:01 -0300

    Adiciona void para seleção de opções

--- main.cpp
+++ main.cpp
@@ -1,26 +1,45 @@
 /*
-      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-      ||                                                    ||
-      ||      |---------------------------------------|     ||
-      ||      |   Tralho de introdução a algoritmos   |     ||
-      ||      |---------------------------------------|     ||
-      ||                                                    ||
-      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-      || Autores:                                           ||
-      ||    Daniel Botelho                                  ||
-      ||    João Vitor Santos Barbosa                       ||
-      ||    Pedro Henrrique Guimarães Carvalho              ||
-      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-      ||                                                    ||
-      ||  Repositório original disponivél no github         ||
-      ||  Acessível pelo link:                              ||
-      ||  https://github.com/JoaoVitorSBarbosa/TrabalhoIALG ||
-      ||                                                    ||
-      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+          ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+          ||                                                    ||
+          ||      |---------------------------------------|     ||
+          ||      |   Tralho de introdução a algoritmos   |     ||
+          ||      |---------------------------------------|     ||
+          ||                                                    ||
+          ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+          || Autores:                                           ||
+          ||    Daniel Botelho                                  ||
+          ||    João Vitor Santos Barbosa                       ||
+          ||    Pedro Henrrique Guimarães Carvalho              ||
+          ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+          ||                                                    ||
+          ||  Repositório original disponivél no github         ||
+          ||  Acessível pelo link:                              ||
+          ||  https://github.com/JoaoVitorSBarbosa/TrabalhoIALG ||
+          ||                                                    ||
+          ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 */
+
 #include <iostream>
 
+using namespace std;
+
+void optionSelect() {
+
+    unsigned int option = 0;
+    cout<< "Bem vindo!" << endl;
+    cout<< "Insira alguma opção" << endl;
+    cout<< "(1)incluir \n(2)excluir \n(3)listar \n(4)alterar\n(5)ordenar \n(6)sair "<< endl;
+    cin >> option;
+    while (option>6)
+    {
+      cout << "Opção inválida, por favor tente novamente" << endl;
+      cin >> option;
+    }
+    
+  return;
+}
 int main () {
+  optionSelect();
   return 0;
 }
