ddf228307115dc73602655170ea34913e6ccda7a
João Vítor <joao.barbosa@tcsindustrial.com.br>
2022-04-11 15:43:14 -0300

    Adiciona cabeçalho

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/*
+      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      ||                                                    ||
+      ||      |---------------------------------------|     ||
+      ||      |   Tralho de introdução a algoritmos   |     ||
+      ||      |---------------------------------------|     ||
+      ||                                                    ||
+      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      || Autores:                                           ||
+      ||    Daniel Botelho                                  ||
+      ||    João Vitor Santos Barbosa                       ||
+      ||    Pedro Henrrique Guimarães Carvalho              ||
+      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+      ||                                                    ||
+      ||  Repositório original disponivél no github         ||
+      ||  Acessível pelo link:                              ||
+      ||  https://github.com/JoaoVitorSBarbosa/TrabalhoIALG ||
+      ||                                                    ||
+      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+*/
 #include <iostream>
 
 int main () {
